1359909a2855690613b0b848e3615223c32bb776
Jonas S Karlsson <jsk@yesco.org>
2026-02-03 21:46:05 +0700

    working on search and put/insert

--- ds.c
+++ ds.c
@@ -51,18 +51,22 @@ handle oshandle(char* k, char* c, word ts) {
 
 typedef char PageRef[4];
 
+#define PAGE_ENTRIES 20
+
 typedef struct Page {
   PageRef next, up; // 8 B
   char ofree; // 1
   char n; // 1
   //char reserved[6];
-  char data[256-8-2-20]; // start at 10 ??? or 16
+  char data[256-8-2-PAGE_ENTRIES]; // start at 10 ??? or 16
   // offsets of ordered prefixed compressed entries
   // bigger grows down
-  char offsets[20];
+  char offsets[PAGE_ENTRIES];
 } Page;
 
 #define PAGE_OFREE_OFFSET offsetof(Page, data)
+#define PAGE_DATA_LEN (256-offsetof(Page, data)-PAGE_OFREE_OFFSET)
+
 Page page = {"NEXT", "UBER",
   PAGE_OFREE_OFFSET+26+13+14, 1,
   // (- 256 8 2 20) = 226
@@ -104,6 +108,9 @@ void printkey(char* k) {
   while(len--) printchar(*++k);
 }
 
+void applykey(char* key, char* p) {
+}
+
 void printentry(char* k, char* p) {
   char
     * start= p,
@@ -142,12 +149,87 @@ void printpage(Page *page) {
   while(*o) printentry(k, p+*o--);
 }
 
+void pagesplit(Page *p, char i, char o, handle h) {
+  // TODO: does this return new page?
+  // who inserts the item? (and where?)
+
+  // How to make it split where lots of inserts
+  // are happening?
+  // The idea is to make hotspot (append location)
+  // be really efficient, preferring it to be at end?
+  // (actually, insering "before first" in new page
+  //  may be more  efficient? - no need search much!)
+  // if stuff after is substantia, maybe move that to
+  // a third page?
+}
+
+Page* ospage(handle h) {
+  // TODO:
+  return &page;
+}
+
+// Is "next key" greater than the Handle key?
+// (don't apply as we can't use it for prefix calc)
+char keygt(char* o, handle h) {
+  // TODO: be clever!
+  return 1;
+}
+
+char pageindex(Page *page, char* key, handle h) {
+  char * p= (void*)page;
+  char i= 0, o;
+
+  while((o= p[--i])) {
+    // once we get a pagekey >= that's the insert point
+    if (keygt(p+o, h)) break;
+    applykey(key, p+o);
+  }
+
+  return i;
+}
+
+char hprefixlen(handle h, char* key, char* o) {
+  // TODO: implement
+  assert(0);
+  return 0;
+}
+
 // TODO: error code/
-void osput(handle h, char* d, word len) {
+void osput(handle h, char t, char* d, word len) {
   // TODO: find right page using index
-  char * p= (void*)&page;
-  
-  // find right entry of combined key h
+  Page * page= ospage(h);
+  char key[256]= {0};
+  char i= pageindex(page, key, h), j;
+  char o= page->ofree, w= o;
+  char * p= (void*)page;
+
+  // prefix compress
+  char byteslen= h[0];
+  char prelen= hprefixlen(h, key, p+p[i]);
+
+  // fits?
+  // (we don't know compression size yet...)
+  // TODO: 
+  if (len+6+byteslen > PAGE_DATA_LEN)
+    pagesplit(page, i, o, h);
+
+  // write entry
+  p[w++]= prelen;
+  p[w++]= byteslen;
+  p[w++]= t;
+  p[w++]= len >> 8;
+  p[w++]= len & 0xff;
+  while(len--) p[w++]= *d++;
+  p[w++]= 0;
+      
+  page->ofree= w;
+
+  // patch it in at i
+  for(j=256-PAGE_ENTRIES; j>i; --j) p[j]= p[j-1];
+  p[i]= o;
+
+  // TODO:
+  //writepage...
 }
 
 // TODO: osappend? lol
@@ -160,9 +242,8 @@ word osget(handle h, word offset, char* buff, word len) {
 int main() {
   assert(sizeof(Page)==256);
   char* h= oshandle("/foo", "", 4711);
-  //osput(h, 1, "FOO", 3);
+//  osput(h, 1, "FOO", 3);
   free(h);
   printpage((void*)&page);
   return 0;
 }
-
